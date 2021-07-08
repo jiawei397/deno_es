a8da0653dcf4ad06173d4812473d716b2e58a215
jia wei <jw397@126.com>
2021-07-08 13:01:16 +0800

    feat(deps): add version

--- deps.ts
+++ deps.ts
@@ -4,6 +4,6 @@ export {
   assert,
   assertEquals,
 } from "https://deno.land/std@0.83.0/testing/asserts.ts";
-export { urlParse } from "https://deno.land/x/url_parse/mod.ts";
+export { urlParse } from "https://deno.land/x/url_parse@1.0.0/mod.ts";
 export { v4 } from "https://deno.land/std@0.99.0/uuid/mod.ts";
 export { delay } from "https://deno.land/std@0.99.0/async/delay.ts";
